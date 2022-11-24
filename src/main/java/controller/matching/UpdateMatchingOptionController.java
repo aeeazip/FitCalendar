7dcbe3caf51bcd39daeb97748a13d5b5f3139a36
yhj0619 <114487966+yhj0619@users.noreply.github.com>
2022-11-25 02:06:46 +0900

    add

--- src/main/java/controller/matching/UpdateMatchingOptionController.java
+++ src/main/java/controller/matching/UpdateMatchingOptionController.java
@@ -1,5 +1,38 @@
 package controller.matching;
 
-public class UpdateMatchingOptionController {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import controller.Controller;
+import controller.exerciser.ExerciserSessionUtils;
+import model.Exerciser;
+import model.service.ExerciserManager;
+import model.service.MatchingManager;
+
+
+//Matching 시작버튼 누름 -> useMatchSvc 버튼 값 변경 
+public class UpdateMatchingOptionController implements Controller {
+	private static final Logger log = LoggerFactory.getLogger(MatchingStartController.class);
+
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response) {
+		// TODO Auto-generated method stub
+		MatchingManager matchingManager = MatchingManager.getInstance();
+		ExerciserManager exManager = ExerciserManager.getInstance();
+
+		HttpSession session = request.getSession();
+		
+		//로그인한 사용자의 exerciser 객체
+		Exerciser exerciser = exManager.findExerciser(ExerciserSessionUtils.getLoginUserId(session));
+		
+		//UseMatchSvc&maxMate 값 변경
+		
+		//실패시, 다시 설정하도록.
+		
+		return "redirect:matching/changeOptions";
+	}
 }
