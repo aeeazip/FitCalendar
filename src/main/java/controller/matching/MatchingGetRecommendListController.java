cef9dd1f0754803cc83f0dd1150fd6df3bb4b38f
inwoo <inwoo0808@gmail.com>
2022-11-25 14:57:35 +0900

    recommend dao, manager update

--- src/main/java/controller/matching/MatchingGetRecommendListController.java
+++ src/main/java/controller/matching/MatchingGetRecommendListController.java
@@ -1,5 +1,24 @@
 package controller.matching;
 
-public class MatchingGetRecommendListController {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import controller.Controller;
+import model.Exerciser;
+import model.service.ExerciserManager;
+
+public class MatchingGetRecommendListController implements Controller {
+	private static final Logger log = LoggerFactory.getLogger(MatchingGetRecommendListController.class);
+
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		
+		
+		
+		return "redirect:/matching/matchingMenu/getRecommendList";
+	}
 
 }
